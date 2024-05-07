abf3f563767e3855258858c07f505afa7e6cceb2
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2024-05-07 16:33:45 +0200

    Snake case fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterpill01/pages/home-page.dart';
+import 'package:flutterpill01/pages/home_page.dart';
 
 void main() {
   runApp(const StoreApp());
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,4 @@
-import 'package:flutterpill01/pages/home-page.dart';
+import 'package:flutterpill01/pages/home_page.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(debugLogDiagnostics: true, routes: [
