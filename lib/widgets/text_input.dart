e16dca9f94eade08ebc841cb7c3c87d62ed55bf0
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2024-05-09 08:48:53 +0200

    Add future and controller in text_input

--- lib/widgets/text_input.dart
+++ lib/widgets/text_input.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 
-class TextInput extends StatelessWidget {
+const keyField = "TextValue";
+
+class TextInput extends StatefulWidget {
   const TextInput({super.key});
 
   @override
+  State<TextInput> createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    //TODO load init data in widget and set the controller
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 250,
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Some text value',
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 1)),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return SizedBox(
+                width: 250,
+                child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Some text value',
+                  ),
+                  // TODO update data using shared preferences
+                  onChanged: (newValue) {},
+                ),
+              );
+          }
+        });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
