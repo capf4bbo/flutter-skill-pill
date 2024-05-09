d6fbc3657e589adeabe5c34a888e66b86a167b9d
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2024-05-09 08:51:45 +0200

    Add future to switch button

--- lib/widgets/switch_button.dart
+++ lib/widgets/switch_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const keyField = "BoolValue";
+
 class SwitchButton extends StatefulWidget {
   const SwitchButton({super.key});
 
@@ -9,16 +11,38 @@ class SwitchButton extends StatefulWidget {
 
 class _SwitchButtonState extends State<SwitchButton> {
   bool value = true;
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    //TODO load init data in widget
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Switch(
-      value: value,
-      onChanged: (bool newValue) {
-        setState(() {
-          value = newValue;
+    return FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 1)),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return Switch(
+                value: value,
+                // TODO update data using shared preferences
+                onChanged: (bool newValue) {
+                  setState(() {
+                    value = newValue;
+                  });
+                },
+              );
+          }
         });
-      },
-    );
   }
 }
