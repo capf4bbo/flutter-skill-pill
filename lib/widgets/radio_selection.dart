aa65ed9d25f6cc3100a18c262d5f363564ba2746
Eduard Conesa Guerrero <eduard.conesa-guerrero@capgemini.com>
2024-05-09 08:54:32 +0200

    Add FutureBuilder to radio_selection

--- lib/widgets/radio_selection.dart
+++ lib/widgets/radio_selection.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 enum RadioValue { first, second, third }
 
+const String keyField = "RadioField";
+
 class RadioSelection extends StatefulWidget {
   const RadioSelection({super.key});
 
@@ -11,41 +13,63 @@ class RadioSelection extends StatefulWidget {
 
 class _RadioSelectionState extends State<RadioSelection> {
   RadioValue? selectedValue = RadioValue.first;
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    //TODO load init data in widget
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: const Text('First'),
-          leading: Radio<RadioValue>(
-            value: RadioValue.first,
-            groupValue: selectedValue,
-            onChanged: updateValue,
-          ),
-        ),
-        ListTile(
-          title: const Text('Second'),
-          leading: Radio<RadioValue>(
-            value: RadioValue.second,
-            groupValue: selectedValue,
-            onChanged: updateValue,
-          ),
-        ),
-        ListTile(
-          title: const Text('Third'),
-          leading: Radio<RadioValue>(
-            value: RadioValue.third,
-            groupValue: selectedValue,
-            onChanged: updateValue,
-          ),
-        ),
-      ],
-    );
+    return FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 1)),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            case ConnectionState.active:
+            case ConnectionState.done:
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    title: const Text('First'),
+                    leading: Radio<RadioValue>(
+                      value: RadioValue.first,
+                      groupValue: selectedValue,
+                      onChanged: updateValue,
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Second'),
+                    leading: Radio<RadioValue>(
+                      value: RadioValue.second,
+                      groupValue: selectedValue,
+                      onChanged: updateValue,
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Third'),
+                    leading: Radio<RadioValue>(
+                      value: RadioValue.third,
+                      groupValue: selectedValue,
+                      onChanged: updateValue,
+                    ),
+                  ),
+                ],
+              );
+          }
+        });
   }
 
   void updateValue(RadioValue? value) {
     value = value ?? RadioValue.first;
+    // TODO update data using shared preferences
     selectedValue = value;
     setState(() {});
   }
